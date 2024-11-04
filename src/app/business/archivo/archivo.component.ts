7ed157b3cba47eb302a9251587076bb1681a7320
M.S.C. Adolfo Simbron <simbron2803@gmail.com>
2024-11-04 10:45:50 -0600

    Logro: se crea interface

---
+++ src/app/business/archivo/archivo.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-archivo',
+  standalone: true,
+  imports: [ReactiveFormsModule,RouterOutlet],
+  templateUrl: './archivo.component.html',
+  styleUrl: './archivo.component.css'
+})
+export default class ArchivoComponent {
+  form: FormGroup = new FormGroup
+  ({
+   expediente: new FormControl('', Validators.required),
+    nombre: new FormControl('', Validators.required),
+    ap_paterno: new FormControl('', Validators.required),
+    ap_materno: new FormControl('', Validators.required),
+    sexo: new FormControl('', Validators.required),
+    edad: new FormControl('', Validators.required),
+    curp: new FormControl('', Validators.required),
+    domicilio: new FormControl('', Validators.required),
+    telefono: new FormControl('', Validators.required),
+    fecha: new FormControl('', Validators.required),
+    elabora: new FormControl('', Validators.required),
+    ubicacion: new FormControl('', Validators.required),
+    status: new FormControl('', Validators.required),
+  });
+
+  agregarData(){
+   
+    
+      }  
+      resetForm(){
+        this.form.reset();
+      }
+    ageOptions: number[] = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+  
+  
+  
+  }
+
+
---
+++ src/app/business/archivo/interfaces/Paciente.ts
@@ -0,0 +1,20 @@
+export default class Paciente{
+
+      id: number = 0;
+      expediente : string = '';
+      paciente : string = '';
+      ap_paterno : string = '';
+       ap_materno : string = '';
+        sexo : string = '';
+       edad : number = 0;
+      curp: string = '';
+       domicilio: string = '';
+       telefono: string = '';
+      elaboro: string = '';
+       fecha_elab: Date = new Date;
+       ubicacion: string = '';
+    status: string= '';
+
+
+
+}
