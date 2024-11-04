8893d147e6fbba92a56a66728ee9c98206d550ea
Esteisy R <steireyes28@gmail.com>
2024-11-04 13:44:24 -0600

    logro: modificacion de colores en el dashboard

--- src/app/business/dashboard/dashboard.component.ts
+++ src/app/business/dashboard/dashboard.component.ts
@@ -32,12 +32,12 @@ export default class DashboardComponent implements OnInit {
           label: '# of Votes',
           data: [12, 19, 3, 5, 2, 3],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255, 0, 1, 1.0)',
+            'rgba(1, 0, 255, 2.0)',
+            'rgba(255, 255, 1, 2.0)',
+            'rgba(1, 255, 255, 2.0)',
+            'rgba(255, 1, 255, 2.0)',
+            'rgba(0, 255, 1, 2.0)'
           ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
